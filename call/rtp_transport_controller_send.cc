45ad1d0431b89d1f0a23b09d216b899d50853cf9
ashhab7 <m.ashhab614@gmail.com>
2025-06-26 15:45:47 -0400

    updateing the handleTransportPacketFeedback method for removing the flipping behaviour

--- call/rtp_transport_controller_send.cc
+++ call/rtp_transport_controller_send.cc
@@ -680,33 +680,24 @@ void RtpTransportControllerSend::OnCongestionControlFeedback(
   }
 }
 
+//added for removing flipping behaviour
+
 void RtpTransportControllerSend::HandleTransportPacketsFeedback(
     const TransportPacketsFeedback& feedback) {
-  // if (sending_packets_as_ect1_) {
-  //   // If transport does not support ECN, packets should not be sent as ECT(1).
-  //   // TODO: bugs.webrtc.org/42225697 - adapt to ECN feedback and continue to
-  //   // send packets as ECT(1) if transport is ECN capable.
-  //   sending_packets_as_ect1_ = false;
-  //   RTC_LOG(LS_INFO) << " Transport is "
-  //                    << (feedback.transport_supports_ecn ? "" : " not ")
-  //                    << " ECN capable. Stop sending ECT(1).";
-  //   packet_router_.ConfigureForRfc8888Feedback(sending_packets_as_ect1_);
-  // }
-  // if (controller_)
-  //   PostUpdates(controller_->OnTransportPacketsFeedback(feedback));
-
-  // Log ECN support detection
-  if (sending_packets_as_ect1_ && !feedback.transport_supports_ecn) {
-    RTC_LOG(LS_WARNING) << "Transport does NOT support ECN. Disabling ECT(1) marking.";
-  } else if (feedback.transport_supports_ecn) {
+  
+  // Log ECN support detection - but don't flip state constantly
+  if (feedback.transport_supports_ecn) {
     RTC_LOG(LS_INFO) << "Transport confirmed to support ECN.";
-  }
-  else{
-    sending_packets_as_ect1_ = false;
-    RTC_LOG(LS_INFO) << " Transport is "
-                     << (feedback.transport_supports_ecn ? "" : " not ")
-                     << " ECN capable. Stop sending ECT(1).";
-    packet_router_.ConfigureForRfc8888Feedback(sending_packets_as_ect1_);
+    // If transport supports ECN, ensure we're sending ECT(1)
+    if (!sending_packets_as_ect1_) {
+      sending_packets_as_ect1_ = true;
+      packet_router_.ConfigureForRfc8888Feedback(sending_packets_as_ect1_);
+      RTC_LOG(LS_INFO) << "Enabling ECT(1) marking - transport supports ECN.";
+    }
+  } else if (sending_packets_as_ect1_) {
+    // Only log warning when we're actually sending ECT(1) but transport doesn't support it
+    RTC_LOG(LS_WARNING) << "Transport does NOT support ECN while sending ECT(1).";
+    // Note: Don't disable ECT(1) immediately - let it stabilize
   }
   
   // Count CE markings in this feedback
@@ -727,10 +718,63 @@ void RtpTransportControllerSend::HandleTransportPacketsFeedback(
   if (controller_)
     PostUpdates(controller_->OnTransportPacketsFeedback(feedback));
 
-
   // Only update outstanding data if any packet is first time acked.
   UpdateCongestedState();
 }
+
+
+
+// void RtpTransportControllerSend::HandleTransportPacketsFeedback(
+//     const TransportPacketsFeedback& feedback) {
+//   // if (sending_packets_as_ect1_) {
+//   //   // If transport does not support ECN, packets should not be sent as ECT(1).
+//   //   // TODO: bugs.webrtc.org/42225697 - adapt to ECN feedback and continue to
+//   //   // send packets as ECT(1) if transport is ECN capable.
+//   //   sending_packets_as_ect1_ = false;
+//   //   RTC_LOG(LS_INFO) << " Transport is "
+//   //                    << (feedback.transport_supports_ecn ? "" : " not ")
+//   //                    << " ECN capable. Stop sending ECT(1).";
+//   //   packet_router_.ConfigureForRfc8888Feedback(sending_packets_as_ect1_);
+//   // }
+//   // if (controller_)
+//   //   PostUpdates(controller_->OnTransportPacketsFeedback(feedback));
+
+//   // Log ECN support detection
+//   if (sending_packets_as_ect1_ && !feedback.transport_supports_ecn) {
+//     RTC_LOG(LS_WARNING) << "Transport does NOT support ECN. Disabling ECT(1) marking.";
+//   } else if (feedback.transport_supports_ecn) {
+//     RTC_LOG(LS_INFO) << "Transport confirmed to support ECN.";
+//   }
+//   else{
+//     sending_packets_as_ect1_ = false;
+//     RTC_LOG(LS_INFO) << " Transport is "
+//                      << (feedback.transport_supports_ecn ? "" : " not ")
+//                      << " ECN capable. Stop sending ECT(1).";
+//     packet_router_.ConfigureForRfc8888Feedback(sending_packets_as_ect1_);
+//   }
+  
+//   // Count CE markings in this feedback
+//   int ce_count = 0;
+//   for (const auto& packet_result : feedback.packet_feedbacks) {
+//     if (packet_result.ecn == EcnMarking::kCe) {
+//       ce_count++;
+//     }
+//   }
+  
+//   if (ce_count > 0) {
+//     RTC_LOG(LS_INFO) << "Received " << ce_count 
+//                      << " CE-marked packets in feedback batch of " 
+//                      << feedback.packet_feedbacks.size() << " packets";
+//   }
+  
+//   // Forward feedback to congestion controller
+//   if (controller_)
+//     PostUpdates(controller_->OnTransportPacketsFeedback(feedback));
+
+
+//   // Only update outstanding data if any packet is first time acked.
+//   UpdateCongestedState();
+// }
 
 void RtpTransportControllerSend::OnRemoteNetworkEstimate(
     NetworkStateEstimate estimate) {
