cd4cda086b0e25cdc2a0f72c5e4c109f0a81a2e6
ashhab7 <m.ashhab614@gmail.com>
2025-06-26 21:40:33 -0400

    checking time missmacth issue

--- rtc_base/physical_socket_server.cc
+++ rtc_base/physical_socket_server.cc
@@ -377,13 +377,14 @@ int PhysicalSocket::SetOption(Option opt, int value) {
     // significant.
     dscp_ = value << 2;
     value = dscp_ + (ecn_ & kEcnMask);
+    RTC_LOG(LS_INFO) << value;
   } else if (opt == OPT_SEND_ECN) {
     // L4S OVERRIDE: Force ECT(1) marking for L4S unless explicitly disabled
     if (value != 0) {
-      RTC_LOG(LS_INFO) << value;
       value = 1;  // Always use ECT(1) for L4S
       RTC_LOG(LS_INFO) << "L4S override: Forcing ECN to ECT(1)";
     }
+    
     ecn_ = value;
     value = dscp_ + (ecn_ & kEcnMask);
     RTC_LOG(LS_INFO) << "Socket setting outgoing ECN marking: "
