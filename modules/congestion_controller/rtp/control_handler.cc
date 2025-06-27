466e104a2c54e69b6e2cf64857526d514677e37d
ashhab7 <m.ashhab614@gmail.com>
2025-06-26 22:33:44 -0400

    checking time missmacth issue

--- modules/congestion_controller/rtp/control_handler.cc
+++ modules/congestion_controller/rtp/control_handler.cc
@@ -29,9 +29,7 @@ void CongestionControlHandler::SetTargetRate(
   RTC_DCHECK_RUN_ON(&sequenced_checker_);
 
   RTC_LOG(LS_INFO) << "=== TRACE: SetTargetRate called === new_target_rate="
-                   << new_target_rate << ", at_time=" << new_target_rate.at_time
-                   << ", target_rate_at_time="
-                   << new_target_rate.at_time.IsFinite();
+                   << new_target_rate << ", at_time=" << new_target_rate.at_time.IsFinite();
 
   // Capture up to 20 stack frames
   void* callstack[20];
