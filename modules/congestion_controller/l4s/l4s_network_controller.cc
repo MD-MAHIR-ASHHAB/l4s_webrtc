1ac30a039134acc4ffe1bcf5505f741c6ae45899
ashhab7 <m.ashhab614@gmail.com>
2025-07-04 16:09:11 -0400

    incorporating newer congestion control opt1

--- modules/congestion_controller/l4s/l4s_network_controller.cc
+++ modules/congestion_controller/l4s/l4s_network_controller.cc
@@ -1176,8 +1176,7 @@ void L4SNetworkController::ProcessEcnFeedback(
 
   for (const auto& packet : feedback.packet_feedbacks) {
     RTC_LOG(LS_INFO) << "ProcessEcnFeedback: Packet seq=" 
-                     << (packet.sent_packet.sequence_number.has_value() ? 
-                         packet.sent_packet.sequence_number.value() : -1)
+                     << packet.sent_packet.sequence_number
                      << " ECN=" << static_cast<int>(packet.ecn) << " ("
                      << (packet.ecn == EcnMarking::kNotEct ? "NotECT" :
                          (packet.ecn == EcnMarking::kEct0 ? "ECT(0)" :
