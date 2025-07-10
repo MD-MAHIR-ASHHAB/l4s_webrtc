48836c2e96c205652a112bdc3d1e463815e586e8
ashhab7 <m.ashhab614@gmail.com>
2025-07-10 16:34:01 -0400

    somewhat working last version

--- rtc_base/async_udp_socket.cc
+++ rtc_base/async_udp_socket.cc
@@ -107,8 +107,8 @@ int AsyncUDPSocket::SendTo(const void* pv,
                                        options.ecn_1 ? 1 : 0);
     if (set_result == 0) {
       has_set_ect1_options_ = options.ecn_1;
-      RTC_LOG(LS_INFO) << "SOCKET SEND: Successfully set ECN socket option to " 
-                       << (options.ecn_1 ? "ECT(1)" : "Not ECT");
+      // RTC_LOG(LS_INFO) << "SOCKET SEND: Successfully set ECN socket option to " 
+      //                  << (options.ecn_1 ? "ECT(1)" : "Not ECT");
     } else {
       RTC_LOG(LS_ERROR) << "SOCKET SEND: FAILED to set ECN socket option! Error=" << set_result
                         << " Requested=" << (options.ecn_1 ? "ECT(1)" : "Not ECT");
@@ -118,13 +118,13 @@ int AsyncUDPSocket::SendTo(const void* pv,
   int ret = socket_->SendTo(pv, cb, addr);
   
   // Log the result of the send operation
-  if (ret == static_cast<int>(cb)) {
-    RTC_LOG(LS_INFO) << "SOCKET SEND: SUCCESS - Sent " << ret << " bytes with ECN=" 
-                     << (has_set_ect1_options_ ? "ECT(1)" : "Not ECT");
-  } else {
-    RTC_LOG(LS_ERROR) << "SOCKET SEND: FAILED - Attempted " << cb << " bytes, sent " << ret 
-                      << " bytes, error=" << socket_->GetError();
-  }
+  // if (ret == static_cast<int>(cb)) {
+  //   RTC_LOG(LS_INFO) << "SOCKET SEND: SUCCESS - Sent " << ret << " bytes with ECN=" 
+  //                    << (has_set_ect1_options_ ? "ECT(1)" : "Not ECT");
+  // } else {
+  //   RTC_LOG(LS_ERROR) << "SOCKET SEND: FAILED - Attempted " << cb << " bytes, sent " << ret 
+  //                     << " bytes, error=" << socket_->GetError();
+  // }
   SignalSentPacket(this, sent_packet);
   return ret;
 }
