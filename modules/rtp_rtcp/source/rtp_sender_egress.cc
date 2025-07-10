848924661271f7bfbbf79d0a77ee6dbddafdc5ef
ashhab7 <m.ashhab614@gmail.com>
2025-07-10 15:19:45 -0400

    pure l4s prague extreme v3

--- modules/rtp_rtcp/source/rtp_sender_egress.cc
+++ modules/rtp_rtcp/source/rtp_sender_egress.cc
@@ -303,17 +303,10 @@ void RtpSenderEgress::CompleteSendPacket(const Packet& compound_packet,
                                         packet->Ssrc());
   }
   options.send_as_ect1 = packet->send_as_ect1();
-
-  if (packet->send_as_ect1()) {
-    options.ecn = EcnMarking::kEct1;
-  } else {
-    options.ecn = EcnMarking::kNotEct;
-  }
   
 
   RTC_LOG(LS_INFO) << "RtpSenderEgress: Sending packet with id/seq="
                << options.packet_id.value_or(0)
-               << ", ssrc=" << packet->Ssrc()
                << ", type=" << RtpPacketMediaTypeToString(*packet->packet_type())
                << ", size=" << packet->size()
                << ", send_as_ect1=" << (options.send_as_ect1 ? "TRUE" : "FALSE");
