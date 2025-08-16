219b1b9b4bf0ad092d8c191c4322cf335d3dea22
ashhab7 <m.ashhab614@gmail.com>
2025-08-15 22:32:31 -0400

    immediate feedback deployment: major change: l4s state machine step 5

--- pc/l4s_ecn_feedback_adapter.cc
+++ pc/l4s_ecn_feedback_adapter.cc
@@ -76,7 +76,6 @@ void L4sEcnFeedbackAdapter::OnNonCePacketReceived(Timestamp timestamp,
     
     // Capture needed variables for the async call
     L4sImmediateFeedbackController* controller = l4s_controller_;
-    uint64_t callback_count = l4s_callbacks_sent_;
     
     task_queue_->PostTask([controller, timestamp, ssrc, sequence_number]() {
       controller->OnNonCePacketReceived(timestamp, ssrc, sequence_number);
