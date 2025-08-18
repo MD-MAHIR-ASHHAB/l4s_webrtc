da5efd961da27dc7ded3340bd89f666dec7cc32b
ashhab7 <m.ashhab614@gmail.com>
2025-08-18 19:06:23 -0400

    immediate feedback deployment: deploying prague style AIMD

--- examples/peerconnection/client/conductor.cc
+++ examples/peerconnection/client/conductor.cc
@@ -98,9 +98,9 @@ class DummySetSessionDescriptionObserver
 
 std::unique_ptr<TestVideoCapturer> CreateCapturer(
     webrtc::TaskQueueFactory& task_queue_factory) {
-  const size_t kWidth = 1280;
-  const size_t kHeight = 720;
-  const size_t kFps = 60;
+  const size_t kWidth = 640;
+  const size_t kHeight = 480;
+  const size_t kFps = 30;
   std::unique_ptr<webrtc::VideoCaptureModule::DeviceInfo> info(
       webrtc::VideoCaptureFactory::CreateDeviceInfo());
   if (!info) {
