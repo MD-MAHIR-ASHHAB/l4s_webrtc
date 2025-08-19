4f76cfda5071bfe1168297c7d1372eb3bab92a7c
ashhab7 <m.ashhab614@gmail.com>
2025-08-18 20:22:14 -0400

    immediate feedback deployment: deploying prague style AIMD: Packet loss investigation + configure for shorter history window+switching condition of feedback processing updated

--- modules/congestion_controller/l4s/l4s_network_controller.cc
+++ modules/congestion_controller/l4s/l4s_network_controller.cc
@@ -537,7 +537,6 @@ webrtc::NetworkControlUpdate  webrtc::L4SNetworkController::OnTransportLossRepor
       RTC_LOG(LS_INFO) << "L4S: Transport loss report received: "
                    << "Packets lost delta: " << msg.packets_lost_delta
                    << ", Packets received delta: " << msg.packets_received_delta
-                   << ", Total packets: " << total_packets;
     DataRate current_rate = target_rate_.value_or(DataRate::KilobitsPerSec(300));
     capacity_estimator_->OnPacketLoss(current_rate, msg.receive_time);
   }
