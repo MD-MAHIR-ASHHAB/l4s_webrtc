59a93b44bed7673a494163f2cee33f4700b2bfcd
ashhab7 <m.ashhab614@gmail.com>
2025-12-17 17:05:22 -0500

    pushing full hd resolution and frame rate

--- examples/peerconnection/client/conductor.cc
+++ examples/peerconnection/client/conductor.cc
@@ -94,9 +94,9 @@ class DummySetSessionDescriptionObserver
 
 std::unique_ptr<TestVideoCapturer> CreateCapturer(
     webrtc::TaskQueueFactory& task_queue_factory) {
-  const size_t kWidth = 2560;   // 2K resolution (2560x1440)
-  const size_t kHeight = 1440;
-  const size_t kFps = 60;       // 60 FPS
+  const size_t kWidth = 1920;   // (1920x1080)
+  const size_t kHeight = 1080;
+  const size_t kFps = 30;       // 30 FPS
   std::unique_ptr<webrtc::VideoCaptureModule::DeviceInfo> info(
       webrtc::VideoCaptureFactory::CreateDeviceInfo());
   if (!info) {
