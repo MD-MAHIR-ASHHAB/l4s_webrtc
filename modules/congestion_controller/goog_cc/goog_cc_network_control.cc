f4367a88c4360c5f0635af33554e998401e6a106
ashhab7 <m.ashhab614@gmail.com>
2026-06-02 17:44:46 -0400

    periodic writing enabled

--- modules/congestion_controller/goog_cc/goog_cc_network_control.cc
+++ modules/congestion_controller/goog_cc/goog_cc_network_control.cc
@@ -1080,7 +1080,7 @@ void GCCMetricsCollector::LogPeriodicSummary(Timestamp at_time) {
                                   {{"stat_type", "std_dev"}, {"metric", "packet_loss"}});
   }
   // Periodically export all metrics to JSON
-  // ExportToJsonFile("gcc_test_c2.json");
+  ExportToJsonFile("gcc_test_c2.json");
 }
 
 
