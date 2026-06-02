69e9a43acff4b33d99060317d5621f880a323175
ashhab7 <m.ashhab614@gmail.com>
2026-06-01 22:20:10 -0400

    immediate pipeline is turned off. this is huge commit. may need to revert later. keep this in mind. also may nedd to apply  on  gcc ecn branches turn 3

--- pc/rtp_transport.cc
+++ pc/rtp_transport.cc
@@ -25,6 +25,8 @@
 #include "media/base/rtp_utils.h"
 #include "modules/rtp_rtcp/include/rtp_header_extension_map.h"
 #include "modules/rtp_rtcp/source/rtp_packet_received.h"
+#include "p2p/base/packet_transport_internal.h"
+#include "pc/session_description.h"
 #include "rtc_base/checks.h"
 #include "rtc_base/containers/flat_set.h"
 #include "rtc_base/copy_on_write_buffer.h"
@@ -208,6 +210,14 @@ void RtpTransport::DemuxPacket(CopyOnWriteBuffer packet,
   RtpPacketReceived parsed_packet(&header_extension_map_);
   parsed_packet.set_arrival_time(arrival_time);
   parsed_packet.set_ecn(ecn);
+
+  bool parse_successful = parsed_packet.Parse(packet);
+  if (!parse_successful) {
+    RTC_LOG(LS_ERROR)
+        << "Failed to parse the incoming RTP packet before demuxing. Drop it.";
+    return;
+  }
+
   // Continue with normal demuxing
   if (!rtp_demuxer_.OnRtpPacket(parsed_packet)) {
     RTC_LOG(LS_VERBOSE) << "Failed to demux RTP packet: "
@@ -327,6 +337,5 @@ void RtpTransport::MaybeSignalReadyToSend() {
     processing_ready_to_send_ = false;
   }
 }
-
 
 }  // namespace webrtc
