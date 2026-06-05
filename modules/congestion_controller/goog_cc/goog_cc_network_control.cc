db7abf3fb519e1859ec5356ede6562d3f993e190
ashhab7 <m.ashhab614@gmail.com>
2026-06-04 21:50:07 -0400

    minor syntax update

--- modules/congestion_controller/goog_cc/goog_cc_network_control.cc
+++ modules/congestion_controller/goog_cc/goog_cc_network_control.cc
@@ -1172,9 +1172,9 @@ void GCCMetricsCollector::ExportToJsonFile(const std::string& filename) {
   fclose(f);
 
 
-  // // --- ADD THIS THROTTLED LOGGING BLOCK ---
-  // export_call_count_++;
-  // RTC_LOG(LS_INFO) << "Exported metrics successfully to " << filename << " (" << export_call_count_ << " times)";
+  // --- ADD THIS THROTTLED LOGGING BLOCK ---
+  export_call_count_++;
+  RTC_LOG(LS_INFO) << "Exported metrics successfully to " << filename << " (" << export_call_count_ << " times)";
 
   // Only print the log every 100th time this function is called
   if (export_call_count_  == 100) {
