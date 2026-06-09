84f12eaf59ceefaa9a53cc080104710d7b72cade
ashhab7 <m.ashhab614@gmail.com>
2026-06-08 21:18:55 -0400

    exportjson renewed with tmp file for contingency

--- examples/peerconnection/client/linux/main_wnd.cc
+++ examples/peerconnection/client/linux/main_wnd.cc
@@ -424,6 +424,7 @@ void GtkMainWnd::OnRowActivated(GtkTreeView* tree_view,
 }
 
 void GtkMainWnd::OnRedraw() {
+  return; //segfault happens in cairo when trying to redraw the video frame, so skipping redraw for now. 
   gdk_threads_enter();
 
   VideoRenderer* remote_renderer = remote_renderer_.get();
