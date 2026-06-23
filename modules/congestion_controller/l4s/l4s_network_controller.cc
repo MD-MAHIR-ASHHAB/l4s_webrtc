c47594b09a0bb98c9e745a35910c6a4fda43c134
ashhab7 <m.ashhab614@gmail.com>
2026-06-22 23:06:38 -0400

    trying a different loss penalty

--- modules/congestion_controller/l4s/l4s_network_controller.cc
+++ modules/congestion_controller/l4s/l4s_network_controller.cc
@@ -202,7 +202,8 @@ void webrtc::PragueCapacityEstimator::OnPacketLoss(DataRate current_rate, Timest
       // GCC explicitly scales the penalty by exactly half of the loss ratio.
       // e.g., 10% loss = 5% cut. 40% loss = 20% cut.
       // It naturally bounds itself (100% loss = 50% cut maximum). No artificial caps needed!
-      double loss_penalty = 0.5 * loss_ratio;
+      // double loss_penalty = 0.5 * loss_ratio;
+      double loss_penalty = 1.0 * loss_ratio;
       
       double retention_factor = 1.0 - loss_penalty;
       DataRate reduced = std::max(current_rate * retention_factor, min_target_rate_);
@@ -634,8 +635,6 @@ webrtc::NetworkControlUpdate webrtc::L4SNetworkController::OnRoundTripTimeUpdate
     // << " last_rtt=" << last_rtt_.ms()
     // << " smooth=" << last_smoothed_rtt_.ms()
     // << " estimated=" << last_estimated_round_trip_time_.ms();
-
-    
 
       // Log RTT metrics.
     if (metrics_enabled_ && metrics_collector_) {
