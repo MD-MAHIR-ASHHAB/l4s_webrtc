024c16b5531f495e42f2b1c3d908eddfce584177
ashhab7 <m.ashhab614@gmail.com>
2026-06-22 22:20:03 -0400

    turning of feedback min rtt calculation and feeding in into prague

--- modules/congestion_controller/l4s/l4s_network_controller.cc
+++ modules/congestion_controller/l4s/l4s_network_controller.cc
@@ -768,25 +768,25 @@ void webrtc::L4SNetworkController::UpdateAllBandwidthEstimators(const TransportP
   UpdateAlrDetector(feedback);
 
   std::vector<PacketResult> received_feedback = feedback.SortedByReceiveTime();
-  if (!received_feedback.empty()) {
-    const Timestamp max_recv_time = received_feedback.back().receive_time;
-    TimeDelta feedback_min_rtt = TimeDelta::PlusInfinity();
-    for (const auto& packet_feedback : received_feedback) {
-      TimeDelta pending_time = max_recv_time - packet_feedback.receive_time;
-      TimeDelta rtt = feedback.feedback_time - packet_feedback.sent_packet.send_time - pending_time;
-      feedback_min_rtt = std::min(feedback_min_rtt, rtt);
-    }
+  // if (!received_feedback.empty()) {
+  //   const Timestamp max_recv_time = received_feedback.back().receive_time;
+  //   TimeDelta feedback_min_rtt = TimeDelta::PlusInfinity();
+  //   for (const auto& packet_feedback : received_feedback) {
+  //     TimeDelta pending_time = max_recv_time - packet_feedback.receive_time;
+  //     TimeDelta rtt = feedback.feedback_time - packet_feedback.sent_packet.send_time - pending_time;
+  //     feedback_min_rtt = std::min(feedback_min_rtt, rtt);
+  //   }
     
-    if (feedback_min_rtt.IsFinite() && !feedback_min_rtt.IsZero()) {
-      last_rtt_ = feedback_min_rtt;
-      TimeDelta safe_rtt = std::max(last_rtt_, TimeDelta::Millis(20));
-      if (base_rtt_.IsInfinite() || safe_rtt < base_rtt_) {
-          base_rtt_ = safe_rtt;
-      }
-      prague_estimator_->UpdateFromRtt(safe_rtt);
-      last_estimated_round_trip_time_ = safe_rtt;
-    }
-  }
+  //   if (feedback_min_rtt.IsFinite() && !feedback_min_rtt.IsZero()) {
+  //     // last_rtt_ = feedback_min_rtt;
+  //     TimeDelta safe_rtt = std::max(last_rtt_, TimeDelta::Millis(20));
+  //     if (base_rtt_.IsInfinite() || safe_rtt < base_rtt_) {
+  //         base_rtt_ = safe_rtt;
+  //     }
+  //     // prague_estimator_->UpdateFromRtt(safe_rtt);
+  //     last_estimated_round_trip_time_ = safe_rtt;
+  //   }
+  // }
   
   if (acked_estimator_) UpdateAckedBitrateEstimator(feedback);
   if (probe_controller_) ProcessRealProbeResults(feedback);
