dcb0c9739112224a7f0688711a076b9f345b4067
ashhab7 <m.ashhab614@gmail.com>
2026-06-24 12:10:53 -0400

    periodic summary has utc timestamp but call to method is turned off

--- modules/congestion_controller/goog_cc/goog_cc_network_control.cc
+++ modules/congestion_controller/goog_cc/goog_cc_network_control.cc
@@ -1187,7 +1187,7 @@ void GoogCcNetworkController::LogPeriodicMetrics(Timestamp at_time) {
   metrics_collector_->LogLossMetrics(at_time, last_loss_fraction_, last_packets_lost_); 
 
   // Log periodic summary
-  metrics_collector_->LogPeriodicSummary(at_time);
+  // metrics_collector_->LogPeriodicSummary(at_time);
 }
 
 
