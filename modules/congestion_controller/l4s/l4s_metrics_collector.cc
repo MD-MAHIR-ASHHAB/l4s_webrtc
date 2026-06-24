7e71a500e3d985b3db6c6be7c3644bc0a188387c
ashhab7 <m.ashhab614@gmail.com>
2026-06-24 12:23:24 -0400

    periodic summary has utc timestamp and turn on

--- modules/congestion_controller/l4s/l4s_metrics_collector.cc
+++ modules/congestion_controller/l4s/l4s_metrics_collector.cc
@@ -168,7 +168,7 @@ void L4SMetricsCollector::LogPeriodicSummary(Timestamp at_time) {
         "acked_rate_avg_mbps", test_case_name_, throughput_stats_.GetAverage() / 1e6,
         webrtc::test::Unit::kUnitless,
         webrtc::test::ImprovementDirection::kBiggerIsBetter,
-        {{"stat_type", "average"}, {"metric", "acked_rate"}});
+        {{"stat_type", "average"}, {"metric", "acked_rate"},{"timestamp_ms", std::to_string(global_utc_ms)}});
     logger_->LogSingleValueMetric(
         "acked_rate_std_mbps", test_case_name_,
         throughput_stats_.GetStandardDeviation() / 1e6,
